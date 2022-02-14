ab7ffad54dd5fd7714718102b3d5956da82de057
cat88 <w2012@e-mirim.hs.kr>
2022-02-14 17:00:54 +0900

    Object

---
+++ lecture07.js
@@ -0,0 +1,12 @@
+var empty_obj = {};
+typeof(empty_obj);
+
+var man = {name: "홍길동", age:20, height:180}
+
+/*
+man.name => "홍길동"
+man["name"] => "홍길동"\
+man.name = "이몽룡"; => "이몽룡"
+man["age"] = 15; => 15
+man => Object {name" "이몽룡", age: 15, height: 180}
+*/
