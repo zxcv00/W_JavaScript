79aadda33af21b149e558eeabca9973edb3acc21
cat88 <w2012@e-mirim.hs.kr>
2022-02-14 17:09:41 +0900

    undefined와 null

---
+++ lecture08.js
@@ -0,0 +1,15 @@
+var uninitialized_var;
+
+var obj = {
+    x:1,
+    y:2
+};
+
+/*
+obj.z => undefined
+typeof(obj.z) => "undefined"
+var null_var; => undefined
+null_var = null; => null
+null_var; => null
+typeof(null_var); =. "object"
+*/
