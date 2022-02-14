fc1f26cf5344b1c8923ce3b84c84a72484cf766a
cat88 <w2012@e-mirim.hs.kr>
2022-02-14 17:53:45 +0900

    연산자 우선순위

---
+++ lecture10-2.js
@@ -0,0 +1,18 @@
+2 * 3 > 4 + 5 && 6 / 3 == 2 || !false
+
+height >= 180 && gender == "male" || height >= 165 && gender == "female"
+
+(height >= 180 && gender == "male") || (height >= 165 && gender == "female")
+
+
+/*
+우선순위 순으로 정리한 연산자
+1. ++, --
+2. !
+3. *, /, %
+4. +, -
+5. <, <=, >, >=
+6. ==, !=
+7. &&
+8. ||
+*/
