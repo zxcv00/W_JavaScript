c0105a91d5ab6c4453e1c13424c9c1cf2bff1277
cat88 <w2012@e-mirim.hs.kr>
2022-02-14 17:17:27 +0900

    산술연산자

---
+++ lecture09.js
@@ -0,0 +1,14 @@
+var a = 1;
+
+console.log(a);;
+// console.log(a + 1);
+// console.log(++a);
+// console.log(--a);
+console.log(a++);
+console.log(a);
+
+/*
+Math.pow(2, 3); => 8
+Math.sqrt(16); => 4
+Math.random(); => 호출 시 0 ~ 1 사이의 임의의 숫자를 나타냄
+*/
